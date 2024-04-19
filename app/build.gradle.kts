f08915cbe13e04d265beef52dd86905957037ac8
dev_yogesh <yogesh67gurjar@gmail.com>
2024-04-19 23:50:19 +0530

    abcd

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,12 +5,12 @@ plugins {
 
 android {
     namespace = "com.yogesh.simpleloader"
-    compileSdk = 34
+    compileSdk = 33
 
     defaultConfig {
         applicationId = "com.yogesh.simpleloader"
         minSdk = 24
-        targetSdk = 34
+        targetSdk = 33
         versionCode = 1
         versionName = "1.0"
 
--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,9 @@
+buildscript {
+    val agp_version by extra("7.4.0")
+}
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
 plugins {
-    id("com.android.application") version "8.2.0" apply false
+    id("com.android.application") version "7.4.0" apply false
     id("org.jetbrains.kotlin.android") version "1.9.10" apply false
     // hilt
     id("com.google.dagger.hilt.android") version "2.48" apply false
